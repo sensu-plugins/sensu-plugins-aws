db9b2eeb4519358d13423645a7adae80d5adce26
Matt Jones <mattjones@yieldbot.com>
2015-05-21 20:38:51 -0400

    deploy

--- lib/sensu-plugins-aws/version.rb
+++ lib/sensu-plugins-aws/version.rb
@@ -8,7 +8,7 @@ module SensuPluginsAWS
     MINOR = 0
     PATCH = 1
 
-    VER_STRING = [MAJOR, MINOR, PATCH, 'alpha.2'].compact.join('.')
+    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
 
     NAME   = 'sensu-plugins-aws'
     BANNER = "#{NAME} v%s"
