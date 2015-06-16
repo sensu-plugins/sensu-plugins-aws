9dfca825225428cedee8ab21c2d2f9b69d3a6901
Shane Starcher <shanestarcher@gmail.com>
2015-06-16 16:15:19 -0400

    specify region

--- bin/handler-ec2_node.rb
+++ bin/handler-ec2_node.rb
@@ -37,6 +37,9 @@
 # `ec2_node_should_be_deleted?` in your own organization-specific handler, or modify this
 # handler to suit your needs.
 #
+# Requires the following Rubygems (`gem install $GEM`):
+#   - sensu-plugin
+#   - fog
 #
 # Optional a Sensu configuration snippet:
 #   {
