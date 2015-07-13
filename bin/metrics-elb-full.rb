b7eb39bfea7909e9d08c5c849ce403c155702151
Tim Smith <tim@cozy.co>
2015-07-13 15:20:48 -0700

    There's no need to check these vals
    Since there's a default they will always exist in some form. This isn't
    doing what it was actually designed to do

--- bin/metrics-elb-full.rb
+++ bin/metrics-elb-full.rb
@@ -76,7 +76,7 @@ class ELBMetrics < Sensu::Plugin::Metric::CLI::Graphite
 
   def aws_config
     hash = {}
-    hash.update access_key_id: config[:aws_access_key], secret_access_key: config[:aws_secret_access_key] if config[:aws_access_key] && config[:aws_secret_access]
+    hash.update access_key_id: config[:aws_access_key], secret_access_key: config[:aws_secret_access_key]
     hash.update region: config[:aws_region]
     hash
   end
