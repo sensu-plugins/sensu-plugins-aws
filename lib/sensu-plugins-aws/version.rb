4fcf236280f7f8a7c052c48d69be34529cbe7efa
Tim Smith <tim@cozy.co>
2015-08-04 12:57:42 -0700

    deploy

--- lib/sensu-plugins-aws/version.rb
+++ lib/sensu-plugins-aws/version.rb
@@ -5,7 +5,7 @@ module SensuPluginsAWS
   # This defines the version of the gem
   module Version
     MAJOR = 1
-    MINOR = 1
+    MINOR = 2
     PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
