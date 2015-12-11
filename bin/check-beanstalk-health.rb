58e2707f32d22f560bf83274751992a25258ba6c
Leon Gibat <brendan.gibat@gmail.com>
2015-12-11 12:51:35 -0500

    adding in extra info on beanstalk error / warning

--- bin/check-beanstalk-health.rb
+++ bin/check-beanstalk-health.rb
@@ -29,6 +29,7 @@
 
 require 'sensu-plugin/check/cli'
 require 'aws-sdk'
+require 'json'
 
 class BeanstalkHealth < Sensu::Plugin::Check::CLI
 
@@ -46,15 +47,49 @@ class BeanstalkHealth < Sensu::Plugin::Check::CLI
         default: true
 
   def env_health
-    @env_health ||= Aws::ElasticBeanstalk::Client.new
+    @env_health ||= beanstalk_client
       .describe_environment_health({
           environment_name: config[:environment],
           attribute_names: ["Color", "Status", "HealthStatus", "Causes"]
           })
   end
 
+  def beanstalk_client
+    @beanstalk_client ||= Aws::ElasticBeanstalk::Client.new
+  end
+
+  def instances_health
+    @instances_health ||= begin
+      curr_instances = beanstalk_client.describe_instances_health({
+        environment_name: config[:environment],
+        attribute_names: ["Color", "HealthStatus", "Causes"]
+        })
+      instances = curr_instances.instance_health_list
+      while !curr_instances.next_token.nil?
+        curr_instances = beanstalk_client.describe_instances_health({
+          environment_name: config[:environment],
+          attribute_names: ["Color", "HealthStatus", "Causes"]
+          })
+        instances.concat(curr_instances.instance_health_list)
+      end
+      instances
+    end
+  end
+
+  def unhealthy_instances
+    @unhealthy_instances ||= instances_health.select {|i| i.color != "Green"}
+  end
+
   def status_rollup
     "Beanstalk Status: #{env_health.status}, Health Status: #{env_health.health_status}, Causes: #{env_health.causes.join(", ")}"
+  end
+
+  def unhealthy_instance_description(instance)
+    "instance id: #{instance.instance_id}, color: #{instance.color}, health status: #{instance.health_status}, causes: [#{instance.causes.join(", ")}]"
+  end
+
+  def unhealthy_instances_rollup
+    "Unhealthy instances and causes: [ #{unhealthy_instances.collect{|i| unhealthy_instance_description(i)}.join(", ")} ]"
   end
 
   def run
@@ -62,9 +97,9 @@ class BeanstalkHealth < Sensu::Plugin::Check::CLI
     if color == "Green"
       ok
     elsif color == "Yellow"
-      warning "Environment status is YELLOW. #{status_rollup}"
+      warning "Environment status is YELLOW. #{status_rollup} #{unhealthy_instances_rollup}"
     elsif color == "Red"
-      critical "Environment status is RED. #{status_rollup}"
+      critical "Environment status is RED. #{status_rollup} #{unhealthy_instances_rollup}"
     elsif color == "Grey"
       if config[:no_data_ok]
         unknown "Environment status is GREY. This means NO DATA. #{status_rollup}"
