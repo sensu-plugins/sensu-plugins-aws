a79da5fcbe5201d3f0397aa578f9f172b861b8f7
Leon Gibat <brendan.gibat@gmail.com>
2016-01-18 09:34:20 -0500

    merge conflicts again..

--- lib/sensu-plugins-aws/cloudwatch-common.rb
+++ lib/sensu-plugins-aws/cloudwatch-common.rb
@@ -30,11 +30,7 @@ module CloudwatchCommon
       namespace: config[:namespace],
       metric_name: config[:metric_name],
       dimensions: config[:dimensions],
-<<<<<<< HEAD
-      start_time: Time.now - config[:period]*10,
-=======
       start_time: Time.now - config[:period] * 10,
->>>>>>> 539aec13b0d9001c92a4fbfdcd21a98de53efa89
       end_time: Time.now,
       period: config[:period],
       statistics: [config[:statistics]],
@@ -46,36 +42,21 @@ module CloudwatchCommon
     resp = client.get_metric_statistics(metrics_request(config))
 
     no_data = resp_has_no_data(resp, config[:statistics])
-<<<<<<< HEAD
-    if no_data and config[:no_data_ok]
-      ok "#{metric_desc} returned no data but that's ok"
-    elsif no_data and not config[:no_data_ok]
-=======
     if no_data && config[:no_data_ok]
       ok "#{metric_desc} returned no data but that's ok"
     elsif no_data && !config[:no_data_ok]
->>>>>>> 539aec13b0d9001c92a4fbfdcd21a98de53efa89
       unknown "#{metric_desc} could not be retrieved"
     end
 
     value = read_value(resp, config[:statistics])
     base_msg = "#{metric_desc} is #{value}: comparison=#{config[:compare]}"
 
-<<<<<<< HEAD
-    if compare value, config[:critical], config[:comparison]
-      critical "#{base_msg} threshold=#{config[:critical]}"
-    elsif config[:warning] and compare value, config[:warning], config[:comparison]
-      warning "#{base_msg} threshold=#{config[:warning]}"
-    else
-      ok "#{base_msg}, will alarm at #{config[:warning] != nil ? config[:warning] : config[:critical]}"
-=======
     if compare value, config[:critical], config[:compare]
       critical "#{base_msg} threshold=#{config[:critical]}"
     elsif config[:warning] && compare(value, config[:warning], config[:compare])
       warning "#{base_msg} threshold=#{config[:warning]}"
     else
       ok "#{base_msg}, will alarm at #{!config[:warning].nil? ? config[:warning] : config[:critical]}"
->>>>>>> 539aec13b0d9001c92a4fbfdcd21a98de53efa89
     end
   end
 end
