7669cd428f3b958b0b76cf14f77244390152e4e1
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2016-02-23 18:23:51 +0900

    fix for rubocop warnings

--- bin/check-elasticache-failover.rb
+++ bin/check-elasticache-failover.rb
@@ -1,75 +1,69 @@
 #!/usr/bin/env ruby
 
-require "sensu-plugin/check/cli"
-require "aws-sdk"
+require 'sensu-plugin/check/cli'
+require 'aws-sdk'
 
 class CheckElastiCacheFailover < Sensu::Plugin::Check::CLI
-  VERSION = "0.0.1"
+  VERSION = '0.0.1'.freeze
 
-  option(:profile,
-    description: "Profile name of AWS shared credential file entry.",
-    long:        "--profile PROFILE",
-    short:       "-p PROFILE",
-  )
+  option :profile,
+         description: 'Profile name of AWS shared credential file entry.',
+         long:        '--profile PROFILE',
+         short:       '-p PROFILE'
 
-  option(:region,
-    description: "AWS region.",
-    short:       "-r REGION",
-    long:        "--region REGION",
-  )
+  option :region,
+         description: 'AWS region.',
+         short:       '-r REGION',
+         long:        '--region REGION'
 
-  option(:severity,
-    description: "Critical or Warning.",
-    short:       "-s SEVERITY",
-    long:        "--severity SEVERITY",
-    proc:        :intern.to_proc,
-    default:     :critical,
-  )
+  option :severity,
+         description: 'Critical or Warning.',
+         short:       '-s SEVERITY',
+         long:        '--severity SEVERITY',
+         proc:        :intern.to_proc,
+         default:     :critical
 
-  option(:replication_group,
-    description: "Replication group to check.",
-    long:        "--replication-group ID",
-    short:       "-g ID",
-  )
+  option :replication_group,
+         description: 'Replication group to check.',
+         long:        '--replication-group ID',
+         short:       '-g ID'
 
-  option(:node_group,
-    description: "Node group to check.",
-    long:        "--node-group ID",
-    short:       "-n ID",
-  )
+  option :node_group,
+         description: 'Node group to check.',
+         long:        '--node-group ID',
+         short:       '-n ID'
 
-  option(:primary_node,
-    description: "Cluster name that should be primary.",
-    long:        "--primary-node NAME",
-    short:       "-c NAME",
-  )
+  option :primary_node,
+         description: 'Cluster name that should be primary.',
+         long:        '--primary-node NAME',
+         short:       '-c NAME'
 
   def run
     replication_group = elasticache.client.describe_replication_groups.replication_groups.find do |g|
       g.replication_group_id == config[:replication_group]
     end
 
-    unknown "Replication group not found." if replication_group.nil?
+    unknown 'Replication group not found.' if replication_group.nil?
 
     node_group = replication_group.node_groups.find do |g|
       g.node_group_id == config[:node_group]
     end
 
-    unknown "Node group not found." if node_group.nil?
+    unknown 'Node group not found.' if node_group.nil?
 
     node = node_group.node_group_members.find do |n|
       n.cache_cluster_id == config[:primary_node]
     end
 
-    unknown "Node not found." if node.nil?
+    unknown 'Node not found.' if node.nil?
 
     message = "Node `#{config[:primary_node]}` (in replication group `#{config[:replication_group]}`, node group `#{config[:node_group]}`) is "
 
-    if node.current_role == "primary"
-      message += "`primary`."
+    if node.current_role == 'primary'
+      message += '`primary`.'
       ok message
     else
-      message += "**not** `primary`."
+      message += '**not** `primary`.'
       send config[:severity], message
     end
   end
@@ -93,10 +87,10 @@ class CheckElastiCacheFailover < Sensu::Plugin::Check::CLI
 
   def own_region
     @own_region ||= begin
-      require "net/http"
+      require 'net/http'
 
       timeout 3 do
-        Net::HTTP.get("169.254.169.254", "/latest/meta-data/placement/availability-zone").chop
+        Net::HTTP.get('169.254.169.254', '/latest/meta-data/placement/availability-zone').chop
       end
     rescue
       nil
