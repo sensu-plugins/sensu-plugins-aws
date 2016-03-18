7748f1a6e1dedd03ba238b1b649d9b81aa7fe255
Bryan Absher <bryan@praesid.io>
2016-03-18 10:25:47 -0700

    removed explicit keys in favor of instance permissions

--- bin/metrics-emr-steps.rb
+++ bin/metrics-emr-steps.rb
@@ -38,18 +38,6 @@ class EMRStepMetrics < Sensu::Plugin::Metric::CLI::Graphite
          long: '--scheme SCHEME',
          default: 'sensu.aws.emr'
 
-  option :aws_access_key,
-         short: '-a AWS_ACCESS_KEY',
-         long: '--aws-access-key AWS_ACCESS_KEY',
-         description: "AWS Access Key. Either set ENV['AWS_ACCESS_KEY'] or provide it as an option",
-         default: ENV['AWS_ACCESS_KEY']
-
-  option :aws_secret_access_key,
-         short: '-k AWS_SECRET_KEY',
-         long: '--aws-secret-access-key AWS_SECRET_KEY',
-         description: "AWS Secret Access Key. Either set ENV['AWS_SECRET_KEY'] or provide it as an option",
-         default: ENV['AWS_SECRET_KEY']
-
   option :aws_region,
          short: '-r AWS_REGION',
          long: '--aws-region REGION',
