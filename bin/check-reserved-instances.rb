0cf966f928962c4a2a06f41525e3e8c76acb1265
Olivier Bazoud <olivier.bazoud@gmail.com>
2016-03-23 16:40:28 +0100

    Use include Common

--- bin/check-reserved-instances.rb
+++ bin/check-reserved-instances.rb
@@ -27,31 +27,17 @@
 #
 
 require 'sensu-plugin/check/cli'
+require 'sensu-plugins-aws'
 require 'aws-sdk'
 
 class CheckReservedInstances < Sensu::Plugin::Check::CLI
-  option :aws_access_key,
-         short: '-a AWS_ACCESS_KEY',
-         long: '--aws-access-key AWS_ACCESS_KEY',
-         description: "AWS Access Key. Either set ENV['AWS_ACCESS_KEY'] or provide it as an option",
-         default: ENV['AWS_ACCESS_KEY']
-
-  option :aws_secret_access_key,
-         short: '-k AWS_SECRET_KEY',
-         long: '--aws-secret-access-key AWS_SECRET_KEY',
-         description: "AWS Secret Access Key. Either set ENV['AWS_SECRET_KEY'] or provide it as an option",
-         default: ENV['AWS_SECRET_KEY']
+  include Common
 
   option :aws_region,
          short: '-r AWS_REGION',
          long: '--aws-region REGION',
          description: 'AWS Region (defaults to us-east-1).',
          default: 'us-east-1'
-
-  option :use_iam_role,
-         short: '-u',
-         long: '--use-iam',
-         description: 'Use IAM role authenticiation. Instance must have IAM role assigned for this to work'
 
   option :warning,
          description: 'Warn if expire date is lower age in seconds',
@@ -67,24 +53,11 @@ class CheckReservedInstances < Sensu::Plugin::Check::CLI
          default: 60 * 60 * 24 * 30 * 2,
          proc: proc(&:to_i)
 
-  def aws_config
-    { access_key_id: config[:aws_access_key],
-      secret_access_key: config[:aws_secret_access_key],
-      region: config[:aws_region]
-    }
-  end
-
   def run
-    aws_config = {}
-    if config[:use_iam_role].nil?
-      aws_config[:access_key_id] = config[:aws_access_key]
-      aws_config[:secret_access_key] = config[:aws_secret_access_key]
-    end
-
     reserved_instances_critical = []
     reserved_instances_warning = []
 
-    ec2 = Aws::EC2::Client.new(aws_config.merge!(region: config[:aws_region]))
+    ec2 = Aws::EC2::Client.new
     reserved_instances = ec2.describe_reserved_instances(filters: [{ name: 'state', values: ['active'] }]).reserved_instances
 
     reserved_instances.each do |reserved_instance|
@@ -102,6 +75,6 @@ class CheckReservedInstances < Sensu::Plugin::Check::CLI
       warning "Reserved instances will expire soon - #{reserved_instances_warning}"
     end
 
-    ok
+    ok "#{reserved_instances.size} reserved instances"
   end
 end
