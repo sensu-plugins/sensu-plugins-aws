b0305561ca3ca68a4720ede73390fb9a5712b0cd
Juan Carlos Castillo Cano <juancarlos_castillocano@intuit.com>
2016-04-01 15:42:15 +0100

    Using common library. Removing unused stuffs

--- bin/check-kms-key.rb
+++ bin/check-kms-key.rb
@@ -22,37 +22,26 @@
 #   Warning if KMS key id exists but is not enabled
 #   Ok if KMS key id exists and is enabled
 #   Unknown if no key_id is provided
-#   Unknown if there is any error querying KMS
 #
 # NOTES:
 #
 # LICENSE:
-#   Copyright 2016 github.com/jcastillocano
 #   Released under the same terms as Sensu (the MIT license); see LICENSE
 #   for details.
 #
 
 require 'sensu-plugin/check/cli'
+require 'sensu-plugins-aws'
 require 'aws-sdk'
 
 class CheckKMSKey < Sensu::Plugin::Check::CLI
-  option :aws_access_key,
-         short:       '-a AWS_ACCESS_KEY',
-         long:        '--aws-access-key AWS_ACCESS_KEY',
-         description: "AWS Access Key. Either set ENV['AWS_ACCESS_KEY'] or provide it as an option",
-         default:     ENV['AWS_ACCESS_KEY']
-
-  option :aws_secret_access_key,
-         short:       '-k AWS_SECRET_KEY',
-         long:        '--aws-secret-access-key AWS_SECRET_KEY',
-         description: "AWS Secret Access Key. Either set ENV['AWS_SECRET_KEY'] or provide it as an option",
-         default:     ENV['AWS_SECRET_KEY']
+  include Common
 
   option :aws_region,
          short:       '-r AWS_REGION',
          long:        '--aws-region REGION',
-         description: 'AWS Region (defaults to us-east-1).',
-         default:     'us-east-1'
+         description: 'AWS Region.',
+         default: 'us-east-1'
 
   option :key_id,
          short:       '-k ID',
@@ -60,25 +49,15 @@ class CheckKMSKey < Sensu::Plugin::Check::CLI
          description: 'KMS key identifier',
          default:     nil
 
-  def aws_config
-    { access_key_id: config[:aws_access_key],
-      secret_access_key: config[:aws_secret_access_key],
-      region: config[:aws_region]
-    }
-  end
-
-  def aws_client(opts = {})
-    config = aws_config.merge(opts)
-    @aws_client ||= Aws::KMS::Client.new config
+  def kms_client
+    @kms_client ||= Aws::KMS::Client.new
   end
 
   def check_key(id)
-    return aws_client.describe_key(key_id: id)['key_metadata']['enabled']
+    return kms_client.describe_key(key_id: id)['key_metadata']['enabled']
   rescue Aws::KMS::Errors::NotFoundException
-    puts 'Nop'
     critical 'Key doesnt exist'
   rescue => e
-    puts "Failed to check key #{id}: #{e}"
     unknown "Failed to check key #{id}: #{e}"
   end
 
--- test/bin/check_kms_key_spec.rb
+++ test/bin/check_kms_key_spec.rb
@@ -31,28 +31,10 @@ describe 'CheckKMSKey' do
     @invalid_key = { key_metadata: { key_id: '1234', enabled: false } }
   end
 
-  describe '#aws_config' do
-    it 'should return only region' do
-      check = CheckKMSKey.new
-      config = check.aws_config
-      expect(config).to eq(access_key_id: nil, secret_access_key: nil, region: 'us-east-1')
-    end
-  end
-
-  describe '#aws_client' do
+  describe '#kms_client' do
     it 'should return a client' do
       check = CheckKMSKey.new
-      options = { stub_responses: true }
-      client = check.aws_client(options)
-      expect(client.config.stub_responses).to eq(true)
-      expect(client.config.region).to eq('us-east-1')
-    end
-    it 'should return a client with west region' do
-      check = CheckKMSKey.new
-      options = { stub_responses: true, region: 'us-west-2' }
-      client = check.aws_client(options)
-      expect(client.config.stub_responses).to eq(true)
-      expect(client.config.region).to eq('us-west-2')
+      expect(check.aws_config[:region]).to eq('us-east-1')
     end
   end
 
@@ -60,21 +42,21 @@ describe 'CheckKMSKey' do
     it 'should return true with valid enabled kms key' do
       check = CheckKMSKey.new
       @aws_stub.stub_responses(:describe_key, @valid_key)
-      allow(check).to receive(:aws_client).and_return(@aws_stub)
+      allow(check).to receive(:kms_client).and_return(@aws_stub)
       expect(true).to eq(check.check_key('id'))
     end
 
     it 'should return false with valid disabled kms key' do
       check = CheckKMSKey.new
       @aws_stub.stub_responses(:describe_key, @invalid_key)
-      allow(check).to receive(:aws_client).and_return(@aws_stub)
+      allow(check).to receive(:kms_client).and_return(@aws_stub)
       expect(false).to eq(check.check_key('id'))
     end
 
     it 'should return critical with invalid kms key' do
       check = CheckKMSKey.new
       allow(@aws_stub).to receive(:describe_key).and_raise(Aws::KMS::Errors::NotFoundException.new(nil, 'Error'))
-      allow(check).to receive(:aws_client).and_return(@aws_stub)
+      allow(check).to receive(:kms_client).and_return(@aws_stub)
       response = check.check_key('id')
       expect(response).to eq('triggered critical')
     end
@@ -82,7 +64,7 @@ describe 'CheckKMSKey' do
     it 'should return unknown with random error' do
       check = CheckKMSKey.new
       allow(@aws_stub).to receive(:describe_key).and_raise(RuntimeError.new)
-      allow(check).to receive(:aws_client).and_return(@aws_stub)
+      allow(check).to receive(:kms_client).and_return(@aws_stub)
       response = check.check_key('id')
       expect(response).to eq('triggered unknown')
     end
@@ -90,17 +72,19 @@ describe 'CheckKMSKey' do
 
   describe '#run' do
     it 'should run and exit ok when enabled kms key is passed' do
-      check = CheckKMSKey.new('--key-id 1234'.split(' '))
+      check = CheckKMSKey.new
+			check.config[:key_id] = '1234'
       @aws_stub.stub_responses(:describe_key, @valid_key)
-      allow(check).to receive(:aws_client).and_return(@aws_stub)
+      allow(check).to receive(:kms_client).and_return(@aws_stub)
       response = check.run
       expect(response).to eq('triggered ok')
     end
 
     it 'should run and exit with warning when disabled kms key is passed' do
-      check = CheckKMSKey.new('--key-id 1234'.split(' '))
+      check = CheckKMSKey.new
+			check.config[:key_id] = '1234'
       @aws_stub.stub_responses(:describe_key, @invalid_key)
-      allow(check).to receive(:aws_client).and_return(@aws_stub)
+      allow(check).to receive(:kms_client).and_return(@aws_stub)
       response = check.run
       expect(response).to eq('triggered warning')
     end
