065fa2f7ec5a320f3787838cc0da7a8c4aae1bca
Marc Watts <marcky.sharky@googlemail.com>
2016-05-24 16:27:24 +0100

    Amend `check-ecs-service-health` - Collect all services for given cluster

--- bin/check-ecs-service-health.rb
+++ bin/check-ecs-service-health.rb
@@ -68,7 +68,14 @@ class CheckEcsServiceHealth < Sensu::Plugin::Check::CLI
   # List of requested services or all services registered to the cluster
   def service_list(cluster = 'default', services = nil)
     return services.split ',' if services
-    ecs_client.list_services(cluster: cluster)['service_arns']
+    collect_services(cluster)
+  end
+
+  def collect_services(cluster = 'default', token: nil)
+    response = ecs_client.list_services(cluster: cluster, next_token: token)
+    services = response.service_arns
+    services.push(*collect_services(cluster, token: response.next_token)) if response.next_token
+    services
   end
 
   def service_details(cluster = 'default', services = nil)
