1df424856727ebe3144ff7386a48582338af1274
Arthur Lutz <arthur.lutz@logilab.fr>
2017-11-29 12:16:28 +0100

    Fix help message s/critical/warning/

--- bin/check-cloudwatch-composite-metric.rb
+++ bin/check-cloudwatch-composite-metric.rb
@@ -92,7 +92,7 @@ class CloudWatchCompositeMetricCheck < Sensu::Plugin::Check::CLI
          required: true
 
   option :warning,
-         description: 'Trigger a critical when value is over VALUE',
+         description: 'Trigger a warning when value is over VALUE',
          short: '-w VALUE',
          long: '--warning VALUE',
          proc: proc(&:to_f)
