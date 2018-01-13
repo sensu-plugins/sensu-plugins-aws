bdd49a624f44af7e94af35db58db1d230059fd94
Ben Abrams <me@benabrams.it>
2018-01-13 13:29:01 -0800

    somehow using annotated tokens breaks the code.
    reverted the annotations, disabled the rule for those lines matching cops, and added TODO to look at a later date.
    
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- bin/check-subnet-ip-consumption.rb
+++ bin/check-subnet-ip-consumption.rb
@@ -175,10 +175,12 @@ class CheckSubnetIpConsumption < Sensu::Plugin::Check::CLI
     else
       # Compose alert messages at the configured verbosity
       alert_msg = []
-
-      verbosity0 = '%<subnet> at %<percent>%% [%<vpc>]'
-      verbosity1 = '%<subnet> at %<percent>%% (%<consumed>/%<total>) [%<vpc>]'
-      verbosity2 = '%<subnet> (%<cidr> in %<az>) at %<percent>%% (%<consumed>/%<total>) [%<vpc>]'
+      # TODO: Come back and re-asses Rubocop rule, following it's suggestion actually breaks the code
+      # rubocop:disable Style/FormatStringToken
+      verbosity0 = '%{subnet} at %{percent}%% [%{vpc}]'
+      verbosity1 = '%{subnet} at %{percent}%% (%{consumed}/%{total}) [%{vpc}]'
+      verbosity2 = '%{subnet} (%{cidr} in %{az}) at %{percent}%% (%{consumed}/%{total}) [%{vpc}]'
+      # rubocop:enable Style/FormatStringToken
 
       case config[:verbosity]
       when 0
@@ -208,8 +210,11 @@ class CheckSubnetIpConsumption < Sensu::Plugin::Check::CLI
       end
 
       # Throw critical alert with optional account alias display
-      alert_prefix = '%<count> subnets in %<region> exceeding %<threshold>%% IP consumption threshold: %<alerts>'
-      alert_prefix_with_alias = '%<count> subnets in %<alias> (%<region>) exceeding %<threshold>%% IP consumption threshold: %<alerts>'
+      # TODO: Come back and re-asses Rubocop rule, following it's suggestion actually breaks the code
+      # rubocop:disable Style/FormatStringToken
+      alert_prefix = '%{count} subnets in %{region} exceeding %{threshold}%% IP consumption threshold: %{alerts}'
+      alert_prefix_with_alias = '%{count} subnets in %{alias} (%{region}) exceeding %{threshold}%% IP consumption threshold: %{alerts}'
+      # rubocop:enable Style/FormatStringToken
 
       case config[:show_account_alias]
       when true
