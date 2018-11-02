e79f48fbfcccc092eb162c839d87edfb84feb01e
Ben Abrams <me@benabrams.it>
2018-11-01 20:09:14 -0700

    prep for 13.0.0 release

--- lib/sensu-plugins-aws/version.rb
+++ lib/sensu-plugins-aws/version.rb
@@ -1,7 +1,7 @@
 module SensuPluginsAWS
   module Version
-    MAJOR = 12
-    MINOR = 4
+    MAJOR = 13
+    MINOR = 0
     PATCH = 0
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
